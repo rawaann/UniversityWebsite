509205796d4f399049645e59f8fcdb8092e5ab69
rawaann <rawanahmad977@gmail.com>
2021-01-22 18:08:21 +0200

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+export function closeSide(){
+    document.getElementById("sideBar").style.width = "0px";
+}
+
+export function openSide(){
+    document.getElementById("sideBar").style.width = "75%";
+}
