08428e44da795a5a0479468a2b40b52fc774fa0e
rawaann <rawanahmad977@gmail.com>
2021-01-22 18:05:06 +0200

    Create Requests.js

---
+++ src/components/Requests.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/requests.css"
+import SingleSelect from './SingleSelect'
+
+function Requests() {
+    return (
+        <div className="mainReq">
+            <SingleSelect></SingleSelect>
+        </div>
+    );
+}
+
+export default Requests;
