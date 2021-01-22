7c6017d1900189ef3f0b1c7602e7dbdff6951664
rawaann <rawanahmad977@gmail.com>
2021-01-22 18:06:44 +0200

    Create viewStaffdep.js

---
+++ src/components/viewStaffdep.js
@@ -0,0 +1,228 @@
+import React from "react";
+import { makeStyles, withStyles, useTheme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import PropTypes from "prop-types";
+import TableFooter from "@material-ui/core/TableFooter";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import axios from "axios";
+
+const user = localStorage.getItem("JWT");
+
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+    color:theme.palette.common.black
+  }
+}));
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+        className={classes.btn}
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+const columns = [
+  { id: "id", label: "ID", minWidth: 100, align: "center" },
+  { id: "name", label: "Name", minWidth: 100, align: "center" },
+  { id: "email", label: "E-mail", minWidth: 100, align: "center" },
+  { id: "dep", label: "Department", minWidth: 100, align: "center" },
+  { id: "fac", label: "Faculty", minWidth: 100, align: "center" },
+  { id: "office", label: "Office", minWidth: 100, align: "center" },
+  { id: "role", label: "Role", minWidth: 100, align: "center" }
+];
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    display:"none"
+  },
+  container: {
+    display: "flex",
+  },
+});
+
+export default function ViewStaffdep(props) {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState([]);
+  const disp = props.disp;
+
+  const viewCoverage = React.useCallback(async () => {
+    try {//TODO
+      const response = await axios.post(
+        "http://localhost:3001/instructor/viewStaffdep",
+        { headers: { "token": user } }
+      );
+      setRows(response.data);
+    } catch (error) {
+      setRows([]);
+    }
+  });
+
+  React.useEffect(() => {
+    viewCoverage();
+  }, [viewCoverage]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  return (
+    <Paper className={classes.root} style={{display:disp}}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <StyledTableCell align={column.align}>
+                  {column.label}
+                </StyledTableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, rowIndex) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={rowIndex}>
+                    {columns.map((column, columnIndex) => {
+                      return (
+                        <TableCell
+                          key={rowIndex + "" + columnIndex}
+                          align={column.align}
+                        >
+                          {column.id === "id" && row.id}
+                          {column.id === "name" && row.name}
+                          {column.id === "email" && row.email}
+                          {column.id === "dep" && row.dep}
+                          {column.id === "loc" && row.loc}
+                          {column.id === "fac" && row.fac}
+                          {column.id === "office" && row.office}
+                          {column.id === "role" && "["+row.role+"]"}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[
+                  5,
+                  10,
+                  25,
+                  { label: "All", value:rows.length},
+                ]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+}
